2bd71b826d2c1cb9332e715e8138c4597e38110e
Jayant Kumar <jaykrs@gmail.com>
2026-01-24 22:29:41 +0530

    payment integration

--- src/components/pages/orderDetails/common/ConsumerDetails.jsx
+++ src/components/pages/orderDetails/common/ConsumerDetails.jsx
@@ -2,17 +2,19 @@ import SettingContext from "@/context/settingContext";
 import request from "@/utils/axiosUtils";
 import { CountryAPI } from "@/utils/axiosUtils/API";
 import useFetchQuery from "@/utils/hooks/useFetchQuery"; import Link from "next/link";
-;
+import { PaymentMethod } from "@/utils/constants";
 import { useRouter } from "next/navigation";
 import { useContext, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Card, CardBody, Col, Input, Label, Row } from "reactstrap";
+import axios  from "axios";
 
-const ConsumerDetails = ({ data,  taxData}) => {
+const ConsumerDetails = ({ data, taxData }) => {
   const { convertCurrency } = useContext(SettingContext);
   const { t } = useTranslation("common");
   const router = useRouter();
-  const [paymentMode, setPaymentMode] = useState("offline")
+  const [paymentMode, setPaymentMode] = useState("offline");
+  const [paymentMethod, setPaymentMethod] = useState("");
   const { data: countryData } = useFetchQuery([CountryAPI], () => request({ url: CountryAPI }, router), {
     refetchOnWindowFocus: false,
     select: (res) => res.data.map((country) => ({ id: country.id, name: country.name, state: country.state })),
@@ -39,43 +41,93 @@ const ConsumerDetails = ({ data,  taxData}) => {
     return total.toFixed(2)
   }
 
-      function getItemsTotalPrice() {
-          let totalTax = 0, total = 0;
-          
-          data.items.forEach(el => {
-              let jsonData = el.product.jsonData;
-              let _dd = [];
-              if (!jsonData) {
-                  _dd = [{ discountPercentage: 0, discountAmount: 0, taxId: 0, taxAmount: 0, taxpercent: 0, totalPrice: 0 }];
-                  let _taxId = Number(el.product?.tax);
-                  let _taxpercent = taxData.filter((elm) => Number(elm.id) == _taxId);
-                  _taxpercent = _taxpercent[0]?.value;
-                  let _taxAmt = Number(el.product?.price) * Number(_taxpercent) / 100;
-                 
-                  totalTax = Number(el.quantity) > 0 ? totalTax + _taxAmt * Number(el.quantity) : totalTax;
-                  total = Numbder(el.quantity) > 0 ? total + (Number(el.product?.price - _dd[0].discountAmount) + _taxAmt) * Number(el.quantity) : total;
-              }
-              else {
-                  _dd = jsonData.filter(el => el.orderId == data.id);
-                  if (_dd.length > 0) {
-                      let _taxId = Number(el.product?.tax);
-                      let _taxpercent = taxData.filter((elm) => Number(elm.id) == _taxId);
-                      _taxpercent = _taxpercent[0]?.value;
-                      let _taxAmt = Number(_dd[0].sellingPrice) * Number(_taxpercent) / 100;
-                    
-                      totalTax = Number(el.quantity) > 0 ? totalTax + _taxAmt * Number(el.quantity) : totalTax;
-                      total = Number(el.quantity) > 0 ? total + (Number(el.product?.price - _dd[0].discountAmount) + _taxAmt) * Number(el.quantity) : total;
-                  }
-              }
-          });
-          return {totalTax, total}
+  function getItemsTotalPrice() {
+    let totalTax = 0, total = 0;
+
+    data.items.forEach(el => {
+      let jsonData = el.product.jsonData;
+      let _dd = [];
+      if (!jsonData) {
+        _dd = [{ discountPercentage: 0, discountAmount: 0, taxId: 0, taxAmount: 0, taxpercent: 0, totalPrice: 0 }];
+        let _taxId = Number(el.product?.tax);
+        let _taxpercent = taxData.filter((elm) => Number(elm.id) == _taxId);
+        _taxpercent = _taxpercent[0]?.value;
+        let _taxAmt = Number(el.product?.price) * Number(_taxpercent) / 100;
+
+        totalTax = Number(el.quantity) > 0 ? totalTax + _taxAmt * Number(el.quantity) : totalTax;
+        total = Numbder(el.quantity) > 0 ? total + (Number(el.product?.price - _dd[0].discountAmount) + _taxAmt) * Number(el.quantity) : total;
       }
-      
+      else {
+        _dd = jsonData.filter(el => el.orderId == data.id);
+        if (_dd.length > 0) {
+          let _taxId = Number(el.product?.tax);
+          let _taxpercent = taxData.filter((elm) => Number(elm.id) == _taxId);
+          _taxpercent = _taxpercent[0]?.value;
+          let _taxAmt = Number(_dd[0].sellingPrice) * Number(_taxpercent) / 100;
+
+          totalTax = Number(el.quantity) > 0 ? totalTax + _taxAmt * Number(el.quantity) : totalTax;
+          total = Number(el.quantity) > 0 ? total + (Number(el.product?.price - _dd[0].discountAmount) + _taxAmt) * Number(el.quantity) : total;
+        }
+      }
+    });
+    return { totalTax, total }
+  }
+
+  function getProductName() {
+    let _prdName = "";
+    data.items.forEach(el => {
+      _prdName += el.product.name + ", "
+    })
+    return _prdName;
+  }
+
+  function proceedPayment() {
+    const orderid = "";
+    const amount = getItemsTotalPrice()?.total;
+    const reasonForCollection = "Order Id #" + data.id + " ( " + getProductName() + " ) ";
+    if (paymentMethod == "")
+      alert("Please Select Payment Method");
+    else {
+      var data = JSON.stringify({
+        "orderId": data.id,
+        "amount": amount,
+        "method": [
+          paymentMethod
+        ],
+        "reasonForCollection": reasonForCollection
+      });
+      console.log("benepay", data);
+      var config = {
+        method: 'post',
+        url: process.env.API_PRD_URL +'/payments/benePay/getUrl',
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: data
+      };
+
+      axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+          const payurl = response.data?.realTimePaymentData?.message;
+          if(payurl !== "" && payurl.startsWith("https")) 
+            router.push(payurl);
+          else
+            alert("There is some Imtermittent payment issue , please contact Support");
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+    }
+  }
+
   return (
     <>
       <div className="summary-details my-3">
         <Row>
-          <Col xxl={8} lg={12} md={7}>
+          <Col xxl={6} lg={12} md={6}>
             <Card>
               <CardBody>
                 <h3 className="order-title">{t("ConsumerDetails")}</h3>
@@ -128,7 +180,7 @@ const ConsumerDetails = ({ data,  taxData}) => {
               </CardBody>
             </Card>
           </Col>
-          <Col xxl={4} lg={12} md={5} className="cart-section">
+          <Col xxl={6} lg={12} md={6} className="cart-section">
             <Card className="h-m30">
               <CardBody>
                 <h3 className="order-title">{"summary"}</h3>
@@ -160,6 +212,26 @@ const ConsumerDetails = ({ data,  taxData}) => {
                     <li>
                       {t("Total")} <span>{getItemsTotalPrice()?.total?.toFixed(2)}</span>
                     </li>
+                    {data.status === "APPROVED" &&
+                      <li>
+                        <span className="me-2">Pay :</span>
+                        <select
+                          value={paymentMethod}
+                          label="pay method"
+                          onChange={(value) => { setPaymentMethod(value.target.value) }}
+                          className="form-select form-select-sm w-auto"
+                        >
+                          {PaymentMethod.map((method, index) => (
+                            <option value={method.id}>{method.name}</option>
+                          ))}
+                        </select> </li>}
+                    {data.status === "APPROVED" &&
+                      <li>
+                        <button
+                          className="btn-solid btn btn-transparent"
+                          onClick={proceedPayment}
+                        >Payment</button>
+                      </li>}
                   </ul>
                 </div>
               </CardBody>
--- src/utils/constants/index.jsx
+++ src/utils/constants/index.jsx
@@ -1,4 +1,12 @@
 export const Href = "#";
 export const ImagePath = "/assets/images";
 export const audioFile = '/assets/audio/multi-pop.mp3';
-export const storageURL = process.env.storageURL
+export const storageURL = process.env.storageURL;
+export const PaymentMethod = [
+    {name:"Net Banking", id:"NB"},
+    {name:"Debit Card", id:"DC"},
+    {name:"Credit card", id:"CC"},
+    {name:"Net Banking", id:"NB"},
+    {name:"UPI(Unified Payments Interface)", id:"UPI"},
+    {name:"Offline", id:"OFF"}
+]
