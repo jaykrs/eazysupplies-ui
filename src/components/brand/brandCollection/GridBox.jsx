d2cb6475900fd52303d39b26738cbe078d0b68ba
jaykrs <jaykrs@gmail.com>
2026-02-22 23:46:58 +0530

    remove list view

--- src/components/brand/brandCollection/GridBox.jsx
+++ src/components/brand/brandCollection/GridBox.jsx
@@ -14,9 +14,9 @@ const GridBox = ({ grid, setGrid }) => {
         <li className={` ${grid == 4 ? "active" : ""}`} onClick={() => setGrid(4)}>
           <Image src={`${ImagePath}/icon/4.png`} className="product-4-layout-view" alt="grid image" height={16} width={25} />
         </li>
-        <li className={` ${grid == "list" ? "active" : ""}`} onClick={() => setGrid("list")}>
+        {/* <li className={` ${grid == "list" ? "active" : ""}`} onClick={() => setGrid("list")}>
           <Image src={`${ImagePath}/icon/list.png`} className="product-6-layout-view" alt="grid image" height={12} width={18} />
-        </li>
+        </li> */}
       </ul>
     </div>
   );
